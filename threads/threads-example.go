b9bc6f37e509290de1fbfc268ac66766c62d0bdd
Sangopak Kundu <sangojumech07@gmail.com>
2024-01-06 10:51:23 -0500

    not sure why but the counter works oddly

--- threads/threads-example.go
+++ threads/threads-example.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	//"time"
 )
 
 func main(){
 	fmt.Println("This is from main thread start")
 	messages := make(chan string)
-	go HelloWorld(messages)
+	var counter int;
+	for counter =0; counter <10; counter++ {
+		go HelloWorld(messages, counter)
+		fmt.Println("This is from main with counter ", counter)
+	}
 	fmt.Println("This is from main thread end")
 	//time.Sleep(2*time.Second) //mimicing some other important stuff to happen
 
@@ -19,9 +24,9 @@ func main(){
     
 }
 
-func HelloWorld(messages chan string) {
+func HelloWorld(messages chan string, counter int) {
 	fmt.Println("Hello World from other thread start")
-	messages <- "This is a message from in the inner thread"
+	messages <- "This is a message from in the inner thread" + strconv.Itoa(counter)
 	fmt.Println("Hello World from other thread end")
 	//need to close channel otherwise it creates deadlock as the range operation waits for the channel to close
 	close(messages)
