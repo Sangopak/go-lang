d6f704f59aa066453088f2b430e572e911c8deb7
Sangopak Kundu <sangojumech07@gmail.com>
2024-01-15 10:24:55 -0500

    Adding a failure scenario

--- http-client/http-client_test.go
+++ http-client/http-client_test.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"strings"
 	"testing"
+
 	"github.com/jarcoal/httpmock"
 )
 
+var TestEndpoint string = "http://localhost:8090/hello"
 func TestGetGttpSuccess(t *testing.T) {
 	httpmock.Activate()
   	defer httpmock.DeactivateAndReset()
 
-	// Exact URL match
-	httpmock.RegisterResponder("GET", "http://localhost:8090/hello",
+	// Exact URL match with success
+	httpmock.RegisterResponder("GET", TestEndpoint,
     httpmock.NewStringResponder(200, `{"message":"hello"}`))
 
-	actualResponse, _ := GetHttp("http://localhost:8090/hello")
+	actualResponse, _ := GetHttp(TestEndpoint)
 
 	if actualResponse.Status != "200" {
 		t.Error("Did not get the correct Http status 200")
+	}
+}
+
+func TestGetHttpFailure(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+  	// Exact URL match with an error
+  	httpmock.RegisterResponder("GET", TestEndpoint,
+	httpmock.NewErrorResponder(errors.New("500 Internal Server Error")))
+
+	_, actualError := GetHttp(TestEndpoint)
+	errorMessage := fmt.Sprint(actualError)
+	if !strings.Contains(errorMessage, "500 Internal Server Error"){
+		t.Error("Did not get the 500 error")
 	}
 }
